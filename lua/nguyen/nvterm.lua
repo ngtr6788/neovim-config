3a4a31b03e0edc1a854ff0426832f9debfd2ba2f
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-03-13 18:47:42 -0400

    Allow choosing where direction the terminal goes

--- lua/nguyen/nvterm.lua
+++ lua/nguyen/nvterm.lua
@@ -1,8 +1,11 @@
 require('nvterm').setup()
 
 local toggle_modes = {'n', 't'}
+local directions = { h = 'horizontal', v = 'vertical' }
 
-vim.keymap.set(toggle_modes, '<leader>t', 
-  function () require('nvterm.terminal').toggle('horizontal') end,
-  { noremap = true, silent = true }
-)
+for k, v in pairs(directions) do
+  vim.keymap.set(toggle_modes, '<leader>t' .. k, 
+    function () require('nvterm.terminal').toggle(v) end,
+    { noremap = true, silent = true }
+  )
+end
