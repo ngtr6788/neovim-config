3dfc7588c4c2cfa9cd38382a42d236d469ac2e82
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-06-26 08:01:38 -0400

    Confirm setup autocomplete and remove help from lsp

--- lua/nguyen/lsp.lua
+++ lua/nguyen/lsp.lua
@@ -3,3 +3,13 @@ lsp.preset('recommended')
 
 lsp.setup()
 
+-- You need to setup `cmp` after lsp-zero
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+  }
+})
--- lua/nguyen/treesitter.lua
+++ lua/nguyen/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "lua", "vim" },
+  ensure_installed = { "lua", "vim" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
