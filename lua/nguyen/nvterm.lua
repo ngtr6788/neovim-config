ec9d375ca220c1d9cbf0604b52047d9311ae8d3a
Nguyen Tran <nguyen.tran1072e@gmail.com>
2024-03-23 18:28:14 -0400

    Float terminal

--- lua/nguyen/nvterm.lua
+++ lua/nguyen/nvterm.lua
@@ -1,7 +1,23 @@
-require('nvterm').setup()
+local width = 0.9
+local height = 0.8
+
+require('nvterm').setup({
+  terminals = {
+    type_opts = {
+      float = {
+        relative = 'editor',
+        row = (1 - height) / 2,
+        col = (1 - width) / 2,
+        width = width,
+        height = height,
+        border = "single",
+      },
+    }
+  }
+})
 
 local toggle_modes = {'n', 't'}
-local directions = { h = 'horizontal', v = 'vertical' }
+local directions = { h = 'horizontal', v = 'vertical', f = 'float' }
 
 for k, v in pairs(directions) do
   vim.keymap.set(toggle_modes, '<leader>t' .. k, 
